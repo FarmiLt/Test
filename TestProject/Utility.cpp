6020438fd0ad0610ae3b394c12d1f71a85b0bd29
FarmiL_t <hiro_shore@yahoo.co.jp>
2015-01-21 19:56:10 +0900

    Min, Clamp関数記述

--- TestProject/Utility.cpp
+++ TestProject/Utility.cpp
@@ -4,10 +4,23 @@
 template<typename _T>
 _T Max( const _T& a, const _T& b ){
 	return a > b ? a : b;
-
 }
+
+
 template<typename _T>
-_T _T Add(const _T &a, const _T &b)
+_T Min(const _T& a, const _T& b){
+	return a <= b ? a : b;
+}
+
+
+template<typename _T>
+_T Clamp(const _T& max, const _T& min, const _T& aho){
+	return Min( Max( min, aho ), max );
+}
+
+
+template<typename _T>
+_T Add(const _T &a, const _T &b)
 {
 	return a + b;
 }
