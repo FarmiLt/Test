82239e85026fd7e43adaece580bea6846c7682af
FarmiL_t <hiro_shore@yahoo.co.jp>
2015-01-21 19:22:06 +0900

    初期化

---
+++ TestProject/Utility.h
@@ -0,0 +1,14 @@
+#pragma once
+
+template<typename _T>
+_T Add(const _T &a, const _T &b);
+
+template<typename _T>
+_T Max(const _T &a, const _T &b);
+
+template<typename _T>
+_T Min(const _T &a, const _T &b);
+
+template<typename _T>
+_T Clamp(const _T &max, const _T &min, const _T &aho);
+
