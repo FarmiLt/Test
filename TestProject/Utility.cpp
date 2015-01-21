aadb6b80fc2e41e79e16de161193c8f65a1ec92f
FarmiL_t <hiro_shore@yahoo.co.jp>
2015-01-21 19:49:12 +0900

    ｍｋ

--- TestProject/Utility.cpp
+++ TestProject/Utility.cpp
@@ -1,14 +1,13 @@
 #include "Utility.h"
 
-<<<<<<< HEAD
 
 template<typename _T>
 _T Max( const _T& a, const _T& b ){
 	return a > b ? a : b;
-=======
+
+}
 template<typename _T>
 _T _T Add(const _T &a, const _T &b)
 {
 	return a + b;
->>>>>>> master
 }
